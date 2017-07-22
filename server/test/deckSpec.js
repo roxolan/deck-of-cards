6f4d19d171baa626896f8fe77e677dd15a4fcc51
Viktor Kotusenko <roxolanus@gmail.com>
2017-07-22 10:16:15 -0400

    add 2 unit tests for Deck class

--- server/test/deckSpec.js
+++ server/test/deckSpec.js
@@ -1,2 +1,15 @@
 import { expect } from 'chai';
 import { Deck } from '../src/deck.js';
+
+describe('Deck', () => {
+  it('Should have 52 cards', () => {
+    let deck = new Deck()
+    expect(deck._cards.length).to.equal(52)
+  })
+
+  it('Should not contain duplicates', () => {
+    let deck = new Deck()
+    let flatValues = deck._cards.map(card => `${card.suit}${card.rank}`) // flatten to strings so we can compare using Set
+    expect(deck._cards.length).to.equal(new Set(flatValues).size)
+  });
+})
